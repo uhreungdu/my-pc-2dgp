166d8385307e7bb3c64f138d0aa24c81195b975a
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 19:42:06 +0900

    drill 05 - 9 step
    9stel

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -7,20 +7,26 @@ past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 2
 def move_to_spot(count):
     cx, cy= past_spot[count][0], past_spot[count][1]
     frame = 0
+    diration = 0
 
     while (cx != past_spot[count+1][0] or cy != past_spot[count+1][1]):
         clear_canvas()
         grass.draw(400, 30)
         if (cx < past_spot[count + 1][0]):
             cx += 1
-            character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
+            diration = 0
         if (cx > past_spot[count + 1][0]):
             cx -= 1
-            character.clip_draw(frame * 100, 0, 100, 100, cx, cy)
+            diration = 1
         if (cy < past_spot[count + 1][1]):
             cy += 1
         if (cy > past_spot[count + 1][1]):
             cy -= 1
+        if(diration == 0):
+            character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
+        if(diration == 1):
+            character.clip_draw(frame * 100, 0, 100, 100, cx, cy)
+
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.01)
