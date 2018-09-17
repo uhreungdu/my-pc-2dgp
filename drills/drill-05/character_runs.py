fb0dcf81116839328f45509500fbd839ae38b578
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 15:16:14 +0900

    drill 05 - 5step
    5step

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -2,12 +2,15 @@ from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
-past_spot = []
+past_spot = [10]
 
-def save_spot(x,y):
+
+
+def save_spot(x,y,count):
     sx, sy = x , y
+    past_spot[count] = sx + sy
 
-def move_to_spot(x, y):
+def move_to_spot(x, y, count):
     cx, cy, cl= x, y, 0
     frame = 0
     clear_canvas()
@@ -15,7 +18,8 @@ def move_to_spot(x, y):
     character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
     update_canvas()
     delay(0.05)
-    save_spot(cx,cy)
+    save_spot(cx,cy,count)
+    count += 1
 
 
 def move_first(x,y):
@@ -23,16 +27,17 @@ def move_first(x,y):
 
 
 while True:
-    move_to_spot(203, 535)
-    move_to_spot(132,243)
-    move_to_spot(535,470)
-    move_to_spot(477, 203)
-    move_to_spot(715, 136)
-    move_to_spot(316,225)
-    move_to_spot(510, 92)
-    move_to_spot(692, 518)
-    move_to_spot(682, 336)
-    move_to_spot(712,349)
+    count = 0
+    move_to_spot(203, 535,count)
+    move_to_spot(132,243,count)
+    move_to_spot(535,470,count)
+    move_to_spot(477, 203,count)
+    move_to_spot(715, 136,count)
+    move_to_spot(316,225,count)
+    move_to_spot(510, 92,count)
+    move_to_spot(692, 518,count)
+    move_to_spot(682, 336,count)
+    move_to_spot(712,349,count)
 
     move_first(203, 535)
 
