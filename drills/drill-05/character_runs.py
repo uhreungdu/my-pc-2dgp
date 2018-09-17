67f2f53deab4709c227f2e8c90222e8853464aa2
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 20:09:37 +0900

    drill 05 - 14step
    14step

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -4,6 +4,8 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712,349]]
 
+
+
 def move_to_spot(count):
     cx, cy= past_spot[count][0], past_spot[count][1]
     frame = 0
@@ -12,15 +14,15 @@ def move_to_spot(count):
         while (cx != past_spot[count + 1][0] or cy != past_spot[count + 1][1]):
             clear_canvas()
             grass.draw(400, 30)
-            if (cx < past_spot[count + 1][0]):
+            if (cx < past_spot[count + 1][0]):      #작을시 오른쪽으로
                 cx += 1
                 diration = 0
-            if (cx > past_spot[count + 1][0]):
+            if (cx > past_spot[count + 1][0]):      #cx값이 더 클시 왼쪽으로
                 cx -= 1
                 diration = 1
-            if (cy < past_spot[count + 1][1]):
+            if (cy < past_spot[count + 1][1]):      #cy값이 더 작을시 위로
                 cy += 1
-            if (cy > past_spot[count + 1][1]):
+            if (cy > past_spot[count + 1][1]):      #cy값이 더 클시 아래로
                 cy -= 1
             if (diration == 0):
                 character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
@@ -31,7 +33,7 @@ def move_to_spot(count):
             frame = (frame + 1) % 8
             delay(0.01)
 
-def move_first(count):
+def move_first(count):      # 마지막일시 처음점으로 돌아감
     cx, cy = past_spot[9][0], past_spot[9][1]
     frame = 0
     diration = 0
