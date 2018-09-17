71174e13e14b39d7624601b931113ceb286721e6
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 20:23:55 +0900

    drill 05 - 과제 완성
    총 14번 커밋했습니다.
    https://youtu.be/m9jKBen5GCE

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -4,7 +4,7 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712,349]]
 
-
+#과제 완성
 
 def move_to_spot(count):
     cx, cy= past_spot[count][0], past_spot[count][1]
