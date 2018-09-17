eb92bdbc7543240c3b159cbd4c83f4bd2de43094
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 19:32:34 +0900

    drill 05 - 7step
    여러분 or연산자는 or입니다 참고하세요

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -4,15 +4,11 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712,349]]
 
-
-
-
-
 def move_to_spot(count):
     cx, cy= past_spot[count][0], past_spot[count][1]
     frame = 0
 
-    while (cx != past_spot[count+1][0] & cy != past_spot[count + 1][1]):
+    while (cx != past_spot[count+1][0] or cy != past_spot[count+1][1]):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
@@ -26,28 +22,18 @@ def move_to_spot(count):
             cy -= 1
         update_canvas()
         frame = (frame + 1) % 8
-        delay(0.05)
-
-
-    count += 1
-
+        delay(0.01)
 
 def move_first(x,y):
     cx, cy = x, y
 
 
-while True:
-    count = 0
+for count in range(0, 10):
     move_to_spot(count)
-    move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
-    #move_to_spot(count)
+    count = count + 1
+    if (count == 10):
+        count = 0
+
 
     #move_first(203, 535)
 
