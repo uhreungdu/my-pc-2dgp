b415cd5e78ccd7b6fa076a082fedde685a0d2484
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 14:44:58 +0900

    d

--- drills/drill-04/character_runs.py
+++ drills/drill-04/character_runs.py
@@ -3,27 +3,38 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-x = 0
-frame = 0
+
+
+
+
 while(True):
-    while (x < 800):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
-        update_canvas()
-        frame = (frame + 1) % 8
-        x += 5
-        delay(0.05)
-        get_events()
-    while (x > 0):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
-        update_canvas()
-        frame = (frame + 1) % 8
-        x -= 5
-        delay(0.05)
-        get_events()
+    def move_left():
+        frame, x = 0, 0
+        while (x < 800 - 25):
+            clear_canvas()
+            grass.draw(400, 30)
+            character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x += 5
+            delay(0.05)
+            get_events()
+
+
+    def move_right():
+        frame, x = 0, 800 - 25
+        while (x > 0 + 25):
+            clear_canvas()
+            grass.draw(400, 30)
+            character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x -= 5
+            delay(0.05)
+            get_events()
+    move_left()
+
+    move_right()
 
 close_canvas()
 
