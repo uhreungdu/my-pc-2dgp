28c142b7141bfca0a62e9e7344d045e876b057ba
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 14:58:13 +0900

    drill-05 2step

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -3,14 +3,23 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def move_to_spot():
-    pass
+
+
+
+def move_to_spot(x, y):
+    cx, cy= x, y
+    frame = 0
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
+    update_canvas()
+
 def move_first():
     pass
 
 
 while True:
-    move_to_spot()
+    move_to_spot(203, 535)
     move_first()
 
 close_canvas()
