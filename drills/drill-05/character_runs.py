8ec45dd8620871699d2c94e0da4b77e2c282e7d0
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 14:49:49 +0900

    drill 05 - 1step
    첫번째 커밋

---
+++ drills/drill-05/character_runs.py
@@ -0,0 +1,17 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def move_to_spot():
+    pass
+def move_first():
+    pass
+
+
+while True:
+    move_to_spot()
+    move_first()
+
+close_canvas()
+
