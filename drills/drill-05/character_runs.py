5c2d066891bd03a085fa08d301919433e230fd34
jang hyurk youn(MY notebook) <trg0317@naver.com>
2018-09-17 18:56:00 +0900

    drill 05 - 6step
    6step

--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -2,23 +2,33 @@ from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
-past_spot = [10]
+past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712,349]]
 
 
 
-def save_spot(x,y,count):
-    sx, sy = x , y
-    past_spot[count] = sx + sy
 
-def move_to_spot(x, y, count):
-    cx, cy, cl= x, y, 0
+
+def move_to_spot(count):
+    cx, cy= past_spot[count][0], past_spot[count][1]
     frame = 0
-    clear_canvas()
-    grass.draw(400,30)
-    character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
-    update_canvas()
-    delay(0.05)
-    save_spot(cx,cy,count)
+
+    while (cx != past_spot[count+1][0] & cy != past_spot[count + 1][1]):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
+        if (cx < past_spot[count + 1][0]):
+            cx += 1
+        if (cx > past_spot[count + 1][0]):
+            cx -= 1
+        if (cy < past_spot[count + 1][1]):
+            cy += 1
+        if (cy > past_spot[count + 1][1]):
+            cy -= 1
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+
     count += 1
 
 
@@ -28,18 +38,18 @@ def move_first(x,y):
 
 while True:
     count = 0
-    move_to_spot(203, 535,count)
-    move_to_spot(132,243,count)
-    move_to_spot(535,470,count)
-    move_to_spot(477, 203,count)
-    move_to_spot(715, 136,count)
-    move_to_spot(316,225,count)
-    move_to_spot(510, 92,count)
-    move_to_spot(692, 518,count)
-    move_to_spot(682, 336,count)
-    move_to_spot(712,349,count)
+    move_to_spot(count)
+    move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
+    #move_to_spot(count)
 
-    move_first(203, 535)
+    #move_first(203, 535)
 
 close_canvas()
 
