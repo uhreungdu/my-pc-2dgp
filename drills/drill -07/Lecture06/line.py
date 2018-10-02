4916284defba0c7ba6249d61b36e37e90f0e9a80
jang hyuck youn <trg0317@naver.com>
2018-10-02 16:38:10 +0900

    drill 07 step1

--- drills/drill -07/Lecture06/line.py
+++ drills/drill -07/Lecture06/line.py
@@ -1,66 +1,50 @@
 import turtle
 import random
+from pico2d import *
 
 
-def stop():
-    turtle.bye()
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
 
 
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
+def prepare_move_canvas():
+    pass
 
 
 def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
+    pass
 
-
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
-
-
-def draw_line_basic(p1, p2):
-    # fill here
+def draw_character(p):
     pass
 
 
 def draw_line(p1, p2):
-    # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    for i in range(0,100+1,2):
+        t = i/100
+        x = (1-t) * p1[0] + t*p2[0]
+        y = (1-t) * p1[1] + t*p2[1]
+        draw_character((x,y))
+    draw_character(p2)
 
 
-prepare_turtle_canvas()
+
+
+
+
+
+
+count = 0
+while True:
+
+
+
+
+
+close_canvas()
 
 
 
