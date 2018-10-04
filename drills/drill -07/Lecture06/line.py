73ede7be7dedf87d5b29bd795e849d999260a4c8
jang hyuck youn <trg0317@naver.com>
2018-10-04 23:41:55 +0900

    드릴 실패 및 리소스 하나 찾음

--- drills/drill -07/Lecture06/line.py
+++ drills/drill -07/Lecture06/line.py
@@ -1,44 +1,59 @@
 import turtle
 import random
 from pico2d import *
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
-open_canvas()
-grass = load_image('grass.png')
+open_canvas(KPU_WIDTH,KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+frame = 0
+size = 20
+points = [(random.randint(0, KPU_WIDTH), random.randint(0, 1024)) for
+        i in range(size)]
+n = 1
 
-def prepare_move_canvas():
-    pass
+diration = 0
+def draw_character(p,diaration):
+    if (diaration == 0):
+        character.clip_draw(frame * 100, 100, 100, 100, p[0], p[1])
+    if (diaration == 1):
+        character.clip_draw(frame * 100, 0, 100, 100, p[0], p[1])
 
 
-def draw_big_point(p):
-    pass
 
-def draw_character(p):
-    pass
+
+
+
+
+
 
 
 def draw_line(p1, p2):
-    draw_big_point(p1)
-    draw_big_point(p2)
 
     for i in range(0,100+1,2):
         t = i/100
         x = (1-t) * p1[0] + t*p2[0]
         y = (1-t) * p1[1] + t*p2[1]
-        draw_character((x,y))
-    draw_character(p2)
+        if (p1[0] < p2[0]):
+            diration = 0
+        if (p1[1] > p2[0]):
+            diration = 1
+        draw_character((x,y),diration)
 
 
-
-
-
-
-
-
-count = 0
 while True:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    draw_line(points[n-1], points[n])
+    n = (n + 1) % size
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+
+
+
 
 
 
--- drills/drill-05/character_runs.py
+++ drills/drill-05/character_runs.py
@@ -7,31 +7,7 @@ past_spot = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 2
 #과제 완성- 다시 커밋
 
 def move_to_spot(count):
-    cx, cy= past_spot[count][0], past_spot[count][1]
-    frame = 0
-    diration = 0
-    if(count != 9):
-        while (cx != past_spot[count + 1][0] or cy != past_spot[count + 1][1]):
-            clear_canvas()
-            grass.draw(400, 30)
-            if (cx < past_spot[count + 1][0]):      #작을시 오른쪽으로
-                cx += 1
-                diration = 0
-            if (cx > past_spot[count + 1][0]):      #cx값이 더 클시 왼쪽으로
-                cx -= 1
-                diration = 1
-            if (cy < past_spot[count + 1][1]):      #cy값이 더 작을시 위로
-                cy += 1
-            if (cy > past_spot[count + 1][1]):      #cy값이 더 클시 아래로
-                cy -= 1
-            if (diration == 0):
-                character.clip_draw(frame * 100, 100, 100, 100, cx, cy)
-            if (diration == 1):
-                character.clip_draw(frame * 100, 0, 100, 100, cx, cy)
 
-            update_canvas()
-            frame = (frame + 1) % 8
-            delay(0.01)
 
 def move_first(count):      # 마지막일시 처음점으로 돌아감
     cx, cy = past_spot[9][0], past_spot[9][1]
