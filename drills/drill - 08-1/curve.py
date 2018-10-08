eb0837c13359e79fc4bddd8a0c52b328f2dd50c0
jang hyuck youn <trg0317@naver.com>
2018-10-08 14:39:30 +0900

    drill 08 -1 3step

--- drills/drill - 08-1/curve.py
+++ drills/drill - 08-1/curve.py
@@ -98,6 +98,24 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
                     -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
         draw_point((x, y))
     draw_point(p4)
+    # draw p4 - p5
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+
+        draw_point((x, y))
+    draw_point(p5)
+
+    # draw p5 - p1
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p5[0]+(-4*t**2+4*t)*p1[0]+(2*t**2-t)*p2[0]
+        y = (2*t**2-3*t+1)*p5[1]+(-4*t**2+4*t)*p1[1]+(2*t**2-t)*p2[1]
+        draw_point((x, y))
+    draw_point(p2)
 
 
 
