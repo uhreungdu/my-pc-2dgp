a94d9aa21a2cccd0a8fc4ba0f79d58cd0b5bfa3f
jang hyuck youn <trg0317@naver.com>
2018-10-15 10:42:00 +0900

    charater_move

--- project/charater.py
+++ project/charater.py
@@ -1,34 +1,54 @@
 from pico2d import *
 
 
-
+class ballut:
+    pass
 
 class player:
     def __init__(self):
         self.x, self.y = 100, 500
         self.character = load_image('fight_plane.png')
         self.direction = 1
+        self.direction_sero = 0
+        self.direction_garo = 0
 
     def draw(self):
         self.character.clip_draw(0,150,60,50,self.x,self.y)
 
     def move_left(self):
-        self.x -= (10 *0.05)
+        self.x -= (100 *0.05)
     def move_right(self):
-        self.x += (10 *0.05)
+        self.x += (100 *0.05)
     def move_up(self):
-        self.y += (10 *0.05)
+        self.y += (100 *0.05)
     def move_down(self):
-        self.y -= 10
+        self.y -= (100 *0.05)
+
+    def update(self):
+        if(self.direction_sero == -1):
+            self.move_down()
+        elif(self.direction_sero == 1):
+            self.move_up()
+
+
+        if(self.direction_garo == -1):
+            self.move_left()
+        elif(self.direction_garo == 1):
+            self.move_right()
+
+
+
 
 open_canvas(1280,1024)
 moving = True
 play = player()
-direction = True
+
+
+
+
 
 def handle_events():
     global moving
-    global direction
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -36,27 +56,41 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 if(play.x <1230):
-                    play.move_right()
+                    play.direction_garo = 1
             elif event.key == SDLK_LEFT:
                 if(play.x > 50):
-                    play.move_left()
+                    play.direction_garo = -1
             elif event.key == SDLK_UP:
                 if(play.y < 974):
-                    play.move_up()
+                    play.direction_sero = 1
             elif event.key == SDLK_DOWN:
                 if(play.y > 50):
-                    play.move_down()
+                    play.direction_sero = -1
             elif event.key == SDLK_ESCAPE:
                 moving = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                if(play.x <1230):
+                    play.direction_garo = 0
+            elif event.key == SDLK_LEFT:
+                if(play.x > 50):
+                    play.direction_garo = 0
+            elif event.key == SDLK_UP:
+                if(play.y < 974):
+                    play.direction_sero = 0
+            elif event.key == SDLK_DOWN:
+                if(play.y > 50):
+                    play.direction_sero = 0
+
 
 
 while moving:
+    handle_events()
+    play.update()
     clear_canvas()
-
-
     play.draw()
 
 
     update_canvas()
-    handle_events()
+
     delay(0.05)
